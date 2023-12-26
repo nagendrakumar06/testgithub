345d1bba4c2072d7e24a1f4fb6fbabcd8eba8673
anmay <nagendrakumar06@gmail.com>
2023-12-26 22:25:24 +0530

    Initial commit with new code

---
+++ fibonacci_series.py
@@ -0,0 +1,12 @@
+def generate_fibonacci(n):
+    fib_series = [0, 1]
+
+    while len(fib_series) < n:
+        fib_series.append(fib_series[-1] + fib_series[-2])
+
+    return fib_series[:n]
+
+# Example: Generate the first 10 Fibonacci numbers
+n = 10
+fibonacci_series = generate_fibonacci(n)
+print(f"Fibonacci Series (first {n} numbers): {fibonacci_series}")
